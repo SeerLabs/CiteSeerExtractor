c1426e89df82f200e1a51dd8ef9d42bb95b71460
Madian Khabsa <madian@psu.edu>
2013-05-27 17:41:01 -0400

    Cleaning up API code, and adding pdf to text conversion

--- service.py
+++ service.py
@@ -4,7 +4,51 @@ import os
 import subprocess
 
 urls = ('/metadataparser', 'MetadataParser')
-ROOT_FOLDER="../seersuite-extract/CiteSeerExtractor/"
+ROOT_FOLDER="../seersuite-extract/CiteSeerExtractor/"# there must be a trailing /
+
+
+class Extraction:
+	def extractHeaders(self,path):
+		"""extract headers from text file"""
+		try:
+			headers = subprocess.check_output([ROOT_FOLDER+"bin/getHeader.pl",path])
+			web.debug(headers)
+			return headers
+		except Exception as ex:
+			web.debug(ex)
+	def extractCitations(self,path):
+		"""extract citations from text file"""
+		try:
+			citations = subprocess.check_output([ROOT_FOLDER+"bin/getCitations.pl",path])
+			web.debug(citations)
+			return citations
+		except Exception as ex:
+			web.debug(ex)
+			
+class Util:
+	def handleUpload(self, inObject):
+		"""
+		Handles upload coming from web.input, write it to a temp file, and return the path to that temp file
+		"""
+		web.debug(inObject['myfile'].filename) # This is the filename
+		handler, path = tempfile.mkstemp()
+		f = open(path,'w')
+		f.write(inObject['myfile'].file.read())
+		f.close()
+		web.debug(path)
+		return path
+	def pdf2text(self, path):
+		"""
+		calls pdfbox to convert a pdf file into text file. 
+		returns the path of the text file
+		"""
+		try:
+			subprocess.call(["java", "-jar", ROOT_FOLDER+"pdfbox/pdfbox-app-1.8.1.jar", "ExtractText", 
+			path, path+".txt"])
+			return path+".txt"
+		except Exception as ex:
+			web.debug(ex)
+
 
 class MetadataParser:
 	def GET(self):
@@ -18,24 +62,22 @@ class MetadataParser:
 
 	def POST(self):
 		x = web.input(myfile={})
-		web.debug(x['myfile'].filename) # This is the filename
-		handler, path = tempfile.mkstemp()
-		f = open(path,'w')
-		f.write(x['myfile'].file.read())
-		f.close()
-		web.debug(path)
+		utilities = Util()
+		pdfpath = utilities.handleUpload(x)
+		txtpath = utilities.pdf2text(pdfpath)
 		try:
-			headers = subprocess.check_output([ROOT_FOLDER+"bin/getHeader.pl",path])
-			web.debug(headers)
-			citations = subprocess.check_output([ROOT_FOLDER+"bin/getCitations.pl",path])
-			web.debug(citations)
+			extractor = Extraction()
+			headers = extractor.extractHeaders(pdfpath)
+			citations = extractor.extractCitations(pdfpath)
 			merged = headers + citations
 			response = """<?xml version="1.0" encoding="UTF-8"?>"""
 			response = response + "<CSXAPIMetadata>"
 			response = response + merged
 			response = response + "</CSXAPIMetadata>"
+			os.unlink(pdfpath)
+			os.unlink(txtpath)
 			return response
-		except ex as Exception:
+		except Exception as ex:
 			web.debug(ex)
 			
 		raise web.seeother('/metadataparser')
