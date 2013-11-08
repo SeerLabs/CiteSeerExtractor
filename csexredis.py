7834b1406d61d298798c5398a633244b8dc2c232
Kyle Williams <kwilliams@psu.edu>
2013-11-08 17:19:50 -0500

    Checks if simhash has been seen before

--- csexredis.py
+++ csexredis.py
@@ -27,6 +27,40 @@ class CSExRedis:
 		
 		return subhashes
 	
+	def add_token(self, token, simhash, subhashes):
+		r.set(token, simhash)
+		for subhash in subhashes:
+			r.sadd(str(token+"-sh"), subhash)
+	
+	def check_matches(self, token):
+		simhash = r.get(token)
+		subhashes = r.smembers(str(token+"-sh"))
+		candidates = set([])
+		
+		# Returns a list of candidate similar documents that share a subhash
+		for subhash in subhashes:
+			candidates = candidates.union(r.smembers(subhash))
+		if len(candidates) == 0:
+			return 0
+		
+		# Check Hamming distance with each candidate and keep track of the best
+		first = 64
+		best = ""
+		for candidate in candidates:
+			hamming = self.hamming(simhash, candidate)
+			if hamming < first:
+				first = hamming
+				best = candidate
+		# No good match
+		if (first > 3):
+			return 0
+		# Best match
+		return best
+		
+		
+	def hamming(self, str1, str2):
+		return sum(itertools.imap(str.__ne__, str1, str2))
+
 	
 if  __name__ =='__main__':
 	
@@ -35,4 +69,8 @@ if  __name__ =='__main__':
 	simhash = CSExRedis.simhash(sys.argv[1])
 	print simhash
 	subhashes = CSExRedis.split_simhash(simhash, 3)
+	token = sys.argv[2]
+	CSExRedis.add_token(token, simhash, subhashes)
+	print CSExRedis.check_matches(token)
+	
 	
